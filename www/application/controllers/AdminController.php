6ab5e7d6a7443a2d2ea72ce42f4f8f17836e66ce
alex2041 <valavey@gmail.com>
2014-11-08 08:10:26 +0400

    Update AdminController.php

--- www/application/controllers/AdminController.php
+++ www/application/controllers/AdminController.php
@@ -13,111 +13,111 @@ class AdminController implements IController {
 		$fc = FrontController::getInstance();
 		/* Инициализация модели */
 		$model = new panelModel;
-        $model->login();
+        	$model->login();
 		/* Рендируем вью */
 		$output = $model->render(PANEL);
 		/* Создаем свой боди */
-	$fc->setBody($output);
+		$fc->setBody($output);
 	}
     /* Вывод постов в админке */
     public function articleAction() {
 		$fc = FrontController::getInstance();
-        /* Проверка на сессию */
-        self::log();
+        	/* Проверка на сессию */
+        	self::log();
 		$model = new postModel;
-        $model->fetch_all();
+        	$model->fetch_all();
 		$output = $model->render(PANEL_POST);
-	    $fc->setBody($output);
+	    	$fc->setBody($output);
 	}
     /* Вывод категорий по блокам в админке */
     public function categoryAction() {
 		$fc = FrontController::getInstance();
-        self::log();
+        	self::log();
 		$model = new blockModel;
-        $model->fetch_cat();
-        $model->fetch_block();
+        	$model->fetch_cat();
+        	$model->fetch_block();
 		$output = $model->render(PANEL_CAT);
-	    $fc->setBody($output);
+		$fc->setBody($output);
 	}
     /* Редактирование постов */
     public function uppostAction() {
 		$fc = FrontController::getInstance();
-        self::log();
-        $params = $fc->getParams();
+        	self::log();
+        	$params = $fc->getParams();
 		$model = new catModel();
-        $model->updatepost();
-        $model->fetch_cat();
-        $model->fetch_data($params['id']);
+        	$model->updatepost();
+        	$model->fetch_cat();
+        	$model->fetch_data($params['id']);
 		$output = $model->render(UPDATE_POST);
-        $fc->setBody($output);
+        	$fc->setBody($output);
     }
     /* Редактирование категорий */
     public function upcatAction() {
 		$fc = FrontController::getInstance();
-        self::log();
-        $params = $fc->getParams();
+        	self::log();
+        	$params = $fc->getParams();
 		$model = new blockModel();
-        $model->upcat();
-        $model->cat_id($params['id']);
-        $model->fetch_block();
+        	$model->upcat();
+        	$model->cat_id($params['id']);
+        	$model->fetch_block();
 		$output = $model->render(UPDATE_CAT);
-        $fc->setBody($output);
+        	$fc->setBody($output);
     }
     /* Редактирование блоков */
     public function upblockAction() {
 		$fc = FrontController::getInstance();
-        self::log();
-        $params = $fc->getParams();
+        	self::log();
+        	$params = $fc->getParams();
 		$model = new blockModel();
-        $model->upblock();
-        $model->block_id($params['id']);
+        	$model->upblock();
+        	$model->block_id($params['id']);
 		$output = $model->render(UPDATE_BLOCK);
-        $fc->setBody($output);
+        	$fc->setBody($output);
     }
     
     /* Добавление постов */
     public function addpostAction() {
 		$fc = FrontController::getInstance();
-        self::log();
+        	self::log();
 		$model = new catModel();
-        $model->fetch_cat();
-        $model->addpost();
+        	$model->fetch_cat();
+        	$model->addpost();
 		$output = $model->render(ADD_POST);
-        $fc->setBody($output);
+        	$fc->setBody($output);
     }
     /* Добавление категорий */
     public function addcatAction() {
 		$fc = FrontController::getInstance();
-        self::log(); 
+		self::log(); 
 		$model = new blockModel();
-        $model->addcat();
-        $model->fetch_block();
+        	$model->addcat();
+        	$model->fetch_block();
 		$output = $model->render(ADD_CAT);
-        $fc->setBody($output);
+        	$fc->setBody($output);
     }
     /* Удаление постов */
     public function delpostAction() {
 		$fc = FrontController::getInstance();
-        self::log();
-        $params = $fc->getParams();
+        	self::log();
+        	$params = $fc->getParams();
 		$model = new postModel();
-        $model->deletepost($params['id']);
-        /* Выкидываем на страницу с постами */
-        header('Location: /admin/article');
+        	$model->deletepost($params['id']);
+        	/* Выкидываем на страницу с постами */
+        	header('Location: /admin/article');
     }
     /* Удаление категории */
     public function delcatAction() {
 		$fc = FrontController::getInstance();
-        self::log();
-        $params = $fc->getParams();
+        	self::log();
+        	$params = $fc->getParams();
 		$model = new catModel();
-        $model->deletecategory($params['id']);
-        header('Location: /admin/category');
+        	$model->deletecategory($params['id']);
+        	header('Location: /admin/category');
     }
     /* Убиваем сессию */
     public function logoutAction() {
 		session_start();
-        session_destroy();
-        header('Location: login');
+        	session_destroy();
+        	header('Location: login');
 	}
 }
