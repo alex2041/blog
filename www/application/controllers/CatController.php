63e93f024b8b5f0c10d3e7b682c29a9bf1ba28fb
alex2041 <valavey@gmail.com>
2014-11-08 08:11:25 +0400

    Update CatController.php

--- www/application/controllers/CatController.php
+++ www/application/controllers/CatController.php
@@ -2,14 +2,14 @@
 	class CatController implements IController{
 	   /* Вывод статей по категории */
        public function getAction(){
-	       $fc = FrontController::getInstance();
-           $params = $fc->getParams();
-	       $model = new blockModel();
-           $model->cat_data($params['id']);
-           $model->fetch_cat();
-           $model->fetch_block();
-           $output = $model->render(POST_CAT_FILE);
-           $fc->setBody($output);
+	        $fc = FrontController::getInstance();
+           	$params = $fc->getParams();
+	        $model = new blockModel();
+           	$model->cat_data($params['id']);
+           	$model->fetch_cat();
+           	$model->fetch_block();
+           	$output = $model->render(POST_CAT_FILE);
+           	$fc->setBody($output);
 	   }
 	  }
 ?>
