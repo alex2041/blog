f3d7fc813e2e617284bbcf218c1ca8b1bf43277e
alex2041 <valavey@gmail.com>
2014-11-08 08:12:31 +0400

    Update PostController.php

--- www/application/controllers/PostController.php
+++ www/application/controllers/PostController.php
@@ -2,16 +2,14 @@
 	class PostController implements IController{
         /* Вывод статей по id */
        public function getAction(){
-	       $fc = FrontController::getInstance();
-           $params = $fc->getParams();
-	       $model = new blockModel();
-           $model->fetch_data($params['id']);
-           $model->fetch_cat();
-           $model->fetch_block();
-           $output = $model->render(POST_ID_FILE);
-           $fc->setBody($output);
+	        $fc = FrontController::getInstance();
+           	$params = $fc->getParams();
+	        $model = new blockModel();
+           	$model->fetch_data($params['id']);
+           	$model->fetch_cat();
+           	$model->fetch_block();
+           	$output = $model->render(POST_ID_FILE);
+           	$fc->setBody($output);
 	   }
-       
-       
 	}
 ?>
